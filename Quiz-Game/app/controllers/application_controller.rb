b1ab18e820bafd26cc620d2c246e876cee9b8b11
Saksham Gupta <shucon01@gmail.com>
2017-09-09 01:43:41 +0530

    Updated Gem files for deployment

--- Quiz-Game/app/controllers/application_controller.rb
+++ Quiz-Game/app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+  	render html: "Hello , World!"
+  end
+
+  def bye
+  	render html: "Good Bye!!"
+  end
+  
 end
