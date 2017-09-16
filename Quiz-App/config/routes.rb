cdcb864e25ae0971348ceec8df4495e14b01cc05
Saksham Gupta <shucon01@gmail.com>
2017-09-17 02:03:21 +0530

    Added Signup Functionality

--- Quiz-App/config/routes.rb
+++ Quiz-App/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   get  '/help',    to: 'static_pages#help'
-
   get  '/signup',   to: 'users#new'
-
   root 'static_pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/signup',  to: 'users#create'  
+  resources :users
+
 end
